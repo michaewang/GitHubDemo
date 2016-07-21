f26d7143040ee7f66131cc0ebd0e1c67e0445336
michaewang <wangxuan85324@gmail.com>
2016-07-21 16:41:19 +0800

    test 333

--- app/src/main/java/com/example/spreadtrummichaelwang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/spreadtrummichaelwang/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("michael","test");
         Log.d("michael","to test git add command to git hub");
+        Log.d("michael","33333");
     }
 }
